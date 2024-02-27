0f9d031f9f1a3a4d333e0301616dddbdc5894f88
Pedro Cardenas <pedrocardenasrestrepo@gmail.com>
2024-02-26 23:47:44 -0500

    Tutorial 02

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django import forms 
 from django.http import HttpResponseRedirect
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.views import View
 from django.urls import reverse
+from django.shortcuts import render, redirect, get_object_or_404 
+from .models import Product
+from django import forms
 
 # Create your views here.
 
@@ -39,49 +42,113 @@ class ContactPageView(TemplateView):
         return context
 
 
-class Product: 
-    products = [ 
-        {"id":"1", "name":"TV", "description":"Best TV", "price":500}, 
-        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":800}, 
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":100}, 
-        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":50} 
-    ] 
 
 
 class ProductIndexView(View): 
+
     template_name = 'pages/products/index.html' 
+
+ 
+
     def get(self, request): 
+
         viewData = {} 
+
         viewData["title"] = "Products - Online Store" 
+
         viewData["subtitle"] =  "List of products" 
-        viewData["products"] = Product.products 
+
+        viewData["products"] = Product.objects.all() 
+
+ 
+
         return render(request, self.template_name, viewData) 
 
-class ProductShowView(View):
-    template_name = 'pages/products/show.html'
+ 
 
-    def get(self, request, id):
-        viewData = {}
+ 
 
-        try:
-            product = Product.products[int(id) - 1]
+class ProductShowView(View): 
 
-            viewData["title"] = product["name"] + " - Online Store"
+    template_name = 'pages/products/show.html' 
 
-            viewData["subtitle"] = product["name"] + " - Product information"
+ 
 
-            viewData["product"] = product
+ 
 
-            return render(request, self.template_name, viewData)
-        except IndexError:
-            return HttpResponseRedirect(reverse('home'))
+    def get(self, request, id): 
+
+ 
+
+        # Check if product id is valid 
+
+        try: 
+
+            product_id = int(id) 
+
+            if product_id < 1: 
+
+                raise ValueError("Product id must be 1 or greater") 
+
+            product = get_object_or_404(Product, pk=product_id) 
+
+        except (ValueError, IndexError): 
+
+            # If the product id is not valid, redirect to the home page 
+
+            return HttpResponseRedirect(reverse('home')) 
+
+         
+
+        viewData = {} 
+
+        product = get_object_or_404(Product, pk=product_id) 
+
+        viewData["title"] = product.name + " - Online Store" 
+
+        viewData["subtitle"] =  product.name + " - Product information" 
+
+        viewData["product"] = product 
+
+ 
+
+        return render(request, self.template_name, viewData) 
+
+ 
+
+
+
+  
+
+class ProductListView(ListView): 
+
+    model = Product 
+
+    template_name = 'pages/product_list.html' 
+
+    context_object_name = 'products'  # This will allow you to loop through 'products' in your template 
+
+ 
+
+    def get_context_data(self, **kwargs): 
+
+        context = super().get_context_data(**kwargs) 
+
+        context['title'] = 'Products - Online Store' 
+
+        context['subtitle'] = 'List of products' 
+
+        return context    
 
     
-    
-class ProductForm(forms.Form): 
-    name = forms.CharField(required=True) 
-    price = forms.FloatField(required=True)
-    
+class ProductForm(forms.ModelForm): 
+   
+    class Meta: 
+
+        model = Product 
+
+        fields = ['name', 'price']
+
     def clean_price(self):
         price = self.cleaned_data.get('price')
         if price is not None and  price < 0:
@@ -103,6 +170,7 @@ class ProductCreateView(View):
         form = ProductForm(request.POST)
 
         if form.is_valid():
+            form.save()
             return render(request, 'pages/products/product_created.html')
         else: 
             viewData = {} 
