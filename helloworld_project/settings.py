9c4c2495981f9f636477e45e28cb398709ce06a9
Pedro Cardenas <pedrocardenasrestrepo@gmail.com>
2024-02-27 23:43:46 -0500

    Tutorial 3 + Accounts

--- helloworld_project/settings.py
+++ helloworld_project/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+IMAGE_STORAGE_CLASS = 'pages.utils.ImageLocalStorage'
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/' 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path 
-from .views import homePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import homePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView, ImageViewFactory, ImageViewNoDI
+from .utils import ImageLocalStorage
 
 
 urlpatterns = [ 
@@ -10,5 +11,12 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'), 
     path('products/create/', ProductCreateView.as_view(), name='form'), 
     path('products/<str:id>', ProductShowView.as_view(), name='show'), 
+    path('cart/', CartView.as_view(), name='cart_index'), 
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'), 
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'), 
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'), 
+    path('imagenotdi/', ImageViewNoDI.as_view(), name='imagenodi_index'), 
+    path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
 
 ] 
